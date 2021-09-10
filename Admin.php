4ad966f9f1f93276a412b87af713c304a4c14599
santo1989 <65486217+santo1989@users.noreply.github.com>
2021-09-10 15:54:31 +0600

    update
    admin pannel

--- Admin.php
+++ Admin.php
@@ -24,8 +24,68 @@ require "loginproc.php";
 		<input type="password" name="pass" id="pass" placeholder="Password"></input>
 		<input type="submit" value="LOGIN" name="login" id="login"></input>
 </div>
-</form>
-  
+</form> 
+</div>
+<div class="contain">
+<div class="search">
+   <div class="row">
+   <div class="colume" style="margin-top: 5%;">
+   <div class="row">
+
+   <?php 
+
+     $conn = new mysqli('localhost', 'root', '', 'ecomdb');
+     if(isset($_GET['search'])){
+        $searchKey = $_GET['search'];
+        $sql = "SELECT * FROM pricedb WHERE Name LIKE '%$searchKey%'";
+     }else
+     $sql = "SELECT * FROM pricedb";
+     $result = $conn->query($sql);
+   ?>
+
+   <form action="" method="GET"> 
+     <div class="colume">
+        <input type="text" name="search" class='form-control' placeholder="Search By Name" value=<?php echo @$_GET['search']; ?> > 
+     </div>
+     <div class="button">
+      <button>Search</button>
+     </div>
+   </form>
+
+   <br> 
+   <br>
+</div>
+<table class="table">
+  <tr>
+     <th>PriceID</th>
+     <th>Name</th>
+     <th>Description</th>
+     <th>Price</th>
+     <th>image</th>
+     <th>category</th>
+     <th>Add to cart</th>
+     
+  </tr>
+  <?php while( $row = $result->fetch_object() ): ?>
+  <tr>
+     <td><?php echo $row->PriceID  ?></td>
+     <td><?php echo $row->Name ?></td>
+     <td><?php echo $row->Description ?></td>
+     <td><?php echo $row->Price ?></td>
+     <td><img src="image/<?php echo $row->image?>"></td>
+     <td><?php echo $row->category ?></td>
+     <td><button>Update</button> </br></br> <!--line brack -->
+	 <button>Delete</button>
+	</td>
+  </tr>
+  <?php endwhile; ?>
+</table>
+</div>
+</div>
+</div>
+  </body>
+  </html>
+</div>
 </div>
 </body>
 </html>
